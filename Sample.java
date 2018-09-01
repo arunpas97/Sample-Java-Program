061e5714c2064232e17027e65a1991532593077b
Arunkumar Palani <arunkumarpas002@gmail.com>
2018-09-01 22:16:20 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,15 @@
+class samplee
+	{
+		public void show()
+			{
+				System.out.print("Sample Java Program");
+			}
+	}
+class Sample
+	{
+		public static void main(String a[])
+			{
+				samplee obj=new samplee();
+				obj.show();
+			}
+	}
